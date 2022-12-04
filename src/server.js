2095949e756104e1f1672c1ccaba9c62ea9f5f1e
Jose Yañez <yanezluis264@gmail.com>
2022-12-03 18:29:11 -0600

    b

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ router.get('/user',(req,res)=>{
     return res.status(200).send(user)
 })
 router.get('*',(req,res)=>{
-  return res.status(200).send("TE AMO CORAZON, ¿TE CASAS CONMIGO? okno aun es pronto")
+  return res.status(200).send("PAGINA NO DISPONIBLE")
 })
 app.listen(port, () => {
   console.log(`Server listen in port ${port}`)
